b4206925ade1de848dda12252d2d5985f6b819a4
ushan pramod <125153601+ushandissanayaka@users.noreply.github.com>
2024-06-25 10:31:31 +0530

    Home page-web token

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const HomePage = () => {
+  const getUserData = async () => {
+    try {
+      const res = await axios.post('/api/v1/user/getUserData', {}, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        }
+      });
+      console.log(res.data); // Use the response data
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div>
       <h1>Home page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- server.js
+++ server.js
@@ -1,37 +1,31 @@
-const express = require('express')
-const cors = require('cors')
-const colors = require('colors')
-const moragan = require('morgan')
-const dotenv = require('dotenv')
-const connectDB  = require('./config/db')
+const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
-
-//dotenv config
+// dotenv config
 dotenv.config();
 
-//rest object
-const app = express()
-app.use(cors())
+// rest object
+const app = express();
+app.use(cors());
 
-//middlewares
-app.use(express.json()) 
-app.use(moragan('dev'))
+// middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
-//routes
+// routes
 app.use('/api/v1/user', require("./routes/userRoutes"));
 
-//port
-
-const port = process.env.PORT || 8080
+// port
+const port = process.env.PORT || 8080;
 
 // connect database
-connectDB().then(()=>{
-    app.listen(port, () =>{
-        console.log("connected to DB")
-        console.log(`Server Running in ${process.env.NODE_MODE}Mode on port ${process.env.PORT}`.bgCyan.white)
-    })
-})
-
-
-
-
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log("connected to DB");
+        console.log(`Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`.bgCyan.white);
+    });
+});

66ba96ea0c9d2c9477223252ded1e3f162d79240
bishhen <bishanahennayake@gmail.com>
2024-06-27 21:19:29 +0530

    doctor list 1
    doctor list 1

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -1,7 +1,38 @@
 import Layout from './../../components/Layout'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Users = () => {
+const [users , setUsers] = useState([])
+
+//get users
+const getUsers = async() => {
+    try{
+      const res = await axios.get('/api/v1/admin/getAllUsers',{
+        headers:{
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      if(res.data.success){
+        setUsers(res.data.data);
+      }
+    }catch (error){
+      console.log(error)
+    }
+}
+
+useEffect(() => {
+  getUsers()
+},[])
+
+//antD table col
+const columns = [
+  {
+    title:'Name',
+    dataIndex:'name',
+
+  },
+]
   return (
     <Layout>
         <h1>Users List</h1>

7ea0f2ed7faa8db6e097ee7045fc152d71300d28
ushan pramod <125153601+ushandissanayaka@users.noreply.github.com>
2024-06-28 23:26:15 +0530

    doctor-status

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -35,6 +35,39 @@ const getAllDoctorsController = async(req,res) => {
             error
         })
     }
+};
+
+//account status change
+const changeAccountStatusController = async(req,res) => {
+try {
+
+    const {doctorId, status} = req.body
+    const doctor = await doctorModel.findByIdAndUpdate(doctorId, {status})
+    const user = await userModel.findOne({_id:doctor.userId})
+    const notification = user.notification
+    notification.push({
+        type:'doctor-account-request-update',
+        message:`your Doctor Account Request Has ${status}`,
+        onclickPath:'/notification'
+    })
+    user.isDoctor === 'approved' ? true : false
+    await user.save()
+    res.status(201).send({
+        success:true,
+        message:'Account Status Updated',
+        data: doctor, 
+    })
+    
+} catch (error) {
+    console.log(error)
+    res.status(500).send({
+        success:false,
+        message:'Error in Account status', 
+    })
 }
 
-module.exports = { getAllUsersController, getAllDoctorsController}
+}
+
+
+
+module.exports = { getAllUsersController, getAllDoctorsController, changeAccountStatusController}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/authMiddleware');
-const { getAllUsersController, getAllDoctorsController } = require('../controllers/adminCtrl');
+const { getAllUsersController, getAllDoctorsController,changeAccountStatusController } = require('../controllers/adminCtrl');
 
 const router = express.Router()
 
@@ -10,4 +10,7 @@ router.get('/getAllUsers', authMiddleware, getAllUsersController)
 
 //GET METHOD || DOCTORS
 router.get('/getAllDoctors', authMiddleware, getAllDoctorsController)
+
+//post account status
+router.post('/changeAccountStatus', authMiddleware, changeAccountStatusController)
 module.exports = router
